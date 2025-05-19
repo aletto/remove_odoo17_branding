f73ad36a6f7f5312f05bae0d2c2b8ce406a4bc15
aletto <alettos@gmail.com>
2025-05-19 09:12:20 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,8 +2,8 @@
     "name": "Remove Odoo Branding",
     "version": "17.0.1.0.0",
     "category": "Customization",
-    "summary": "Remueve branding de Odoo del sitio web y plantillas de email",
-    "author": "Tu Nombre o Empresa",
+    "summary": "Removes Odoo branding from website, backend, and email templates",
+    "author": "Andres Letto",
     "depends": ["web", "mail", "website"],
     "data": [
         "views/email_templates.xml",
@@ -11,6 +11,9 @@
     "assets": {
         "web.assets_frontend": [
             "remove_odoo_branding/static/src/xml/remove_odoo_footer.xml",
+        ],
+        "web.assets_backend": [
+            "remove_odoo_branding/static/src/xml/remove_odoo_backend.xml",
         ],
     },
     "installable": True,
